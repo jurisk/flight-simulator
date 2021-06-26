dd014d9af96cedfb9928d25622485c1acb9f79e4
Juris K <jurisk@users.noreply.github.com>
2021-06-26 06:42:42 +0300

    Refactoring

--- src/FlightSimulator.tsx
+++ src/FlightSimulator.tsx
@@ -13,15 +13,15 @@ import {
 import {
     AbstractMesh,
     FreeCamera,
-    KeyboardEventTypes,
     MeshAssetTask,
     TextureAssetTask,
     Scene as BabylonScene,
 } from "@babylonjs/core"
 import "@babylonjs/inspector"
-import {Set} from "immutable"
 import {Nullable} from "@babylonjs/core/types"
 import {Fallback} from "./Fallback"
+import {updateScene} from "./scene-updates"
+import {pressedKeys, updateKeys} from "./keys"
 
 const textureAssets: Task[] = [
     { taskType: TaskType.Texture, url: "assets/textures/earth.jpeg", name: "earth" },
@@ -32,22 +32,11 @@ const textureAssets: Task[] = [
 function FlightSimulatorInner(): JSX.Element {
     const scene = useScene()
 
-    let pressedKeys: Set<string> = Set()
-
     if (scene) {
         scene.debugLayer.show({ embedMode: true }).catch((x) => console.error(x))
 
         // TODO: This needs a click / focus on the canvas before it starts registering - not sure what is the best way to fix
-        scene.onKeyboardObservable.add((kbInfo) => {
-            switch (kbInfo.type) {
-            case KeyboardEventTypes.KEYDOWN:
-                pressedKeys = pressedKeys.add(kbInfo.event.key)
-                break
-            case KeyboardEventTypes.KEYUP:
-                pressedKeys = pressedKeys.remove(kbInfo.event.key)
-                break
-            }
-        })
+        scene.onKeyboardObservable.add(updateKeys)
     }
 
     const assetManagerResult = useAssetManager(textureAssets, {
@@ -68,80 +57,15 @@ function FlightSimulatorInner(): JSX.Element {
     airplane.position = initialAirplanePosition
     airplane.rotation = initialAirplaneRotation
 
-    const LeftRoll = "s"
-    const RightRoll = "f"
-    const LeftRudder = "w"
-    const RightRudder = "r"
-    const PitchUp = "d"
-    const PitchDown = "e"
-    const ThrottleUp = "a"
-    const ThrottleDown = "z"
-
-    let throttle = 0.75
-    let roll = 0
-    let rudder = 0
-    let pitch = 0
-
     useBeforeRender(() => {
         if (scene) {
             scene.setActiveCameraByName("follow-camera")
             const deltaTimeInMillis = scene.getEngine().getDeltaTime()
 
             const airplane = airplaneMesh()
-
-            const control = (name: string): number => pressedKeys.contains(name) ? deltaTimeInMillis : 0
-            const controls = (negative: string, positive: string): number => control(positive) - control(negative)
-            const clamp = (value: number, diff: number, min: number, max: number): number => Math.max(Math.min(value + diff, max), min)
-            const normalize = (value: number): number =>
-                value === 0 ? 0 : (value > 0 ? +1 : -1)
-            const clampWithReversal = (value: number, diff: number, coef: number): number =>
-                diff === 0
-                    ? clamp(value, -deltaTimeInMillis * coef * normalize(value), -1, +1) // return controls back to neutral if not touched
-                    : clamp(value, diff * coef, -1, +1)
-
-            const rotationAmount = Math.PI * deltaTimeInMillis * 0.001
-
-            const rollPos = controls(LeftRoll, RightRoll)
-            roll = clampWithReversal(roll, rollPos, 0.001)
-            airplane.rotate(new Vector3(0, 0, 1), rotationAmount * roll)
-
-            const rudderPos = controls(LeftRudder, RightRudder)
-            rudder = clampWithReversal(rudder, rudderPos, 0.001)
-            const RudderFactor = 0.2
-            airplane.rotate(new Vector3(0, 1, 0), rotationAmount * RudderFactor * rudder)
-
-            const pitchPos = controls(PitchDown, PitchUp)
-            pitch = clampWithReversal(pitch, pitchPos, 0.001)
-            if (pitch < 0) { // we can pitch down worse than we can pitch up
-                airplane.rotate(new Vector3(1, 0, 0), rotationAmount * pitch * 0.05)
-            } else if (pitch > 0) {
-                airplane.rotate(new Vector3(1, 0, 0), rotationAmount * pitch * 0.4)
-            }
-
-            const throttlePos = controls(ThrottleDown, ThrottleUp)
-            const MinThrottle = 0
-            const MaxThrottle = 1
-            throttle = clamp(throttle, throttlePos * 0.001, MinThrottle, MaxThrottle)
-            // TODO: throttle isn't really speed, have to decouple and add stalling
-
-            const forward = new Vector3(0, 0, 1)
-            const SpeedFactor = 0.02
-
-            const direction = airplane
-                .getDirection(forward)
-                .scale(deltaTimeInMillis * SpeedFactor * throttle)
-
-            airplane.position.addInPlace(direction)
-
             const camera = cameraRef.current
-            if (camera) {
-                const followDirection = new Vector3(0, 0.2, -1)
-                const FollowCameraDistance = 20
-                camera.position = airplane.position.add(airplane.getDirection(followDirection).scale(FollowCameraDistance))
-                camera.target = airplane.position
-            } else {
-                console.error("no camera")
-            }
+
+            updateScene(deltaTimeInMillis, airplane, camera, pressedKeys)
         }
     })
 
