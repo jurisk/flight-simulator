e1d0b5766b21022d8e4a0b27033cde19849940a0
Juris K <jurisk@users.noreply.github.com>
2021-07-04 08:34:03 +0300

    Minor clean-up

--- src/FlightSimulator.tsx
+++ src/FlightSimulator.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {
-    Vector3, Scene, ArcRotateCamera, SceneLoader,
+    Vector3, Scene, ArcRotateCamera, SceneLoader, Mesh, AbstractMesh, StandardMaterial,
 } from "@babylonjs/core"
 import SceneComponent from "babylonjs-hook"
 import "./App.css"
@@ -16,6 +16,7 @@ import {gameState, State} from "./state"
 import {CannonJSPlugin} from "@babylonjs/core/Physics/Plugins"
 import {PhysicsImpostor} from "@babylonjs/core/Physics/physicsImpostor"
 import * as CANNON from "cannon"
+import {Color3} from "@babylonjs/core/Maths/math.color"
 window.CANNON = CANNON
 
 export const FlightSimulator = (): JSX.Element => {
@@ -57,16 +58,17 @@ export const FlightSimulator = (): JSX.Element => {
 
         const airplane = await loadAirplane()
 
-        // TODO: unfortunately, MeshImpostor fails as vertex for 0-th indexed mesh is null
-        // airplane.physicsImpostor = new PhysicsImpostor(airplane, PhysicsImpostor.MeshImpostor, { mass: 0, friction: 0, restitution: 0 })
-        // console.log(airplane.physicsImpostor)
-        // airplane.physicsImpostor.registerOnPhysicsCollide(groundImpostor, function(main) {
-        //     // TODO: explosion here
-        //     ((main.object as AbstractMesh).material as StandardMaterial).diffuseColor = new Color3(Math.random(), Math.random(), Math.random())
-        //
-        //     // TODO: unfortunately this crashes with "Uncaught DOMException: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node."
-        //     // gameOver()
-        // })
+        const collisionAirplaneMesh = airplane.children[0] // .root didn't have vertices
+        collisionAirplaneMesh.physicsImpostor = new PhysicsImpostor(collisionAirplaneMesh, PhysicsImpostor.MeshImpostor, { mass: 0, friction: 0, restitution: 0 })
+        collisionAirplaneMesh.physicsImpostor.registerOnPhysicsCollide(ground.physicsImpostor, function(main) {
+            console.log("boom");
+
+            // TODO: explosion here
+            ((main.object as AbstractMesh).material as StandardMaterial).diffuseColor = new Color3(Math.random(), Math.random(), Math.random())
+
+            // TODO: unfortunately this crashes with "Uncaught DOMException: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node."
+            gameOver()
+        })
 
         const followDirection = new Vector3(0, 0.2, -1)
         const FollowCameraDistance = 20
@@ -77,19 +79,19 @@ export const FlightSimulator = (): JSX.Element => {
         const ufo = await loadUfo()
 
         // TODO: just a physics test, remove later
-        // const createBall = function () {
-        //     const ball = Mesh.CreateSphere("s", 8, 8, scene)
-        //     ball.position.y = 500
-        //     ball.position.x = (Math.random() * 100) * ((Math.random() < 0.5) ? -1 : 1)
-        //     ball.position.z = (Math.random() * 100) * ((Math.random() < 0.5) ? -1 : 1)
-        //     ball.physicsImpostor = new PhysicsImpostor(ball, PhysicsImpostor.SphereImpostor, { mass: 1, friction: 0, restitution: 0 })
-        // }
-        //
-        // createBall()
-        // createBall()
-        // createBall()
-        // createBall()
-        // createBall()
+        const createBall = function () {
+            const ball = Mesh.CreateSphere("s", 8, 8, scene)
+            ball.position.y = 500
+            ball.position.x = (Math.random() * 100) * ((Math.random() < 0.5) ? -1 : 1)
+            ball.position.z = (Math.random() * 100) * ((Math.random() < 0.5) ? -1 : 1)
+            ball.physicsImpostor = new PhysicsImpostor(ball, PhysicsImpostor.SphereImpostor, { mass: 1, friction: 0, restitution: 0 })
+        }
+
+        createBall()
+        createBall()
+        createBall()
+        createBall()
+        createBall()
 
         scene.registerBeforeRender(() => {
             if (airplane && ufo) {
